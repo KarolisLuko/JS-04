5cf11be641a6fd3f3b56a4eb150a7c58e8fb06f1
Karolis <karolis.luko@gmail.com>
2021-06-16 22:09:50 +0100

    updete1

---
+++ 03/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function myFunction() {
+    var x = document.createElement("P");
+        var t = document.createTextNode("This is a paragraph.");
+        x.appendChild(t);
+        document.body.appendChild(x);
+      }
+    
+
+  
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+"use strict";
 
 let masyvas = [];
 
 let pridekElementa = ()=>{
-    // masyvas[masyvas.length] = elemtas;
-    //masyvas[0] = elemtas; // unshift -- kas cia toks?
      let gert = document.getElementById("name").value;
      masyvas.push(gert);
      console.log(masyvas);
