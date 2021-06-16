fc4a44e763c7c91686a750e31a7cede31c9c741a
Karolis <karolis.luko@gmail.com>
2021-06-16 13:19:12 +0100

    updete

---
+++ Arrey_spargalke.js
@@ -0,0 +1,47 @@
+let a = [];  //arrey
+let b = ['a','b','c'];
+let c = [1, 2, 3];
+let x = [ () => { console.log('WTF ?')}, { a: 'a', b: 'b'}];
+
+b[0] // => a
+b[0] = 'x';
+
+x[0]() // => () => { console.log('WTF ?')}
+
+for ( let i = 0; i< c.length; i++ ){
+    let naujasEl = c[i];
+}
+
+while( true ){
+
+}
+
+c.forEach((element, index) => {
+    element;
+    index;
+});
+
+calss { 
+    function pavadinimas1(kintamasis1, kintamasis_kitas) {
+
+    };
+    
+    let pavadinimas2 = (kintamasis1, kintamasis_kitas) => {
+    
+    }
+    
+    let obj = {
+        pavadinimas3: () => {
+    
+        }
+    }
+    
+    
+    this.pavadinimas1();
+    this.pavadinimas2();
+    this.obj.pavadinimas3()
+    
+}
+
+
+
---
+++ index.js
@@ -0,0 +1,11 @@
+
+let masyvas = [];
+
+let pridekElementa = ()=>{
+    // masyvas[masyvas.length] = elemtas;
+    //masyvas[0] = elemtas; // unshift -- kas cia toks?
+     let gert = document.getElementById("name").value;
+     masyvas.push(gert);
+     console.log(masyvas);
+}
+
